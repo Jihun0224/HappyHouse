cb8e61b28f8817a902e55deba717db83fe57d9f9
yerimki <55068901+yerimki@users.noreply.github.com>
2022-05-19 19:11:20 +0900

    Update boardSQL.sql
    cascade 설정

--- backend/src/main/resources/res/boardSQL.sql
+++ backend/src/main/resources/res/boardSQL.sql
@@ -2,9 +2,9 @@ use happyhouse;
 
 CREATE TABLE IF NOT EXISTS `happyhouse`.`board` (
   `articleno` INT NOT NULL AUTO_INCREMENT,
-  `userid` VARCHAR(16) NULL DEFAULT NULL,
-  `subject` VARCHAR(100) NULL DEFAULT NULL,
-  `content` VARCHAR(2000) NULL DEFAULT NULL,
+  `userid` VARCHAR(16) NOT NULL,
+  `subject` VARCHAR(100) NOT NULL,
+  `content` VARCHAR(2000) NOT NULL,
   `hit` INT NULL DEFAULT 0,
   `regtime` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
   PRIMARY KEY (`articleno`),
@@ -29,18 +29,25 @@ commit;
 CREATE TABLE IF NOT EXISTS `happyhouse`.`answer` (
   `answerno` INT NOT NULL AUTO_INCREMENT,
   `articleno` INT NOT NULL,
-  `userid` VARCHAR(16) NULL DEFAULT NULL,
-  `content` VARCHAR(2000) NULL DEFAULT NULL,
+  `userid` VARCHAR(16) NOT NULL,
+  `content` VARCHAR(200) NOT NULL,
   `regtime` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
   PRIMARY KEY (`answerno`),
   INDEX `answer_to_board_fk` (`articleno` ASC) VISIBLE,
   CONSTRAINT `answer_to_board_fk`
     FOREIGN KEY (`articleno`)
     REFERENCES `happyhouse`.`board` (`articleno`)
+    ON DELETE CASCADE,
+  CONSTRAINT `answer_to_member_fk`
+    FOREIGN KEY (`userid`)
+    REFERENCES `happyhouse`.`member` (`userid`)
     ON DELETE CASCADE)
 ENGINE = InnoDB
 AUTO_INCREMENT = 1
 DEFAULT CHARACTER SET = utf8mb4
 COLLATE = utf8mb4_0900_ai_ci;
 
-select * from board;
+insert into answer(articleno, userid, content) 
+values(1, 'admin', '11월 추천드립니다.');
+
+commit;
