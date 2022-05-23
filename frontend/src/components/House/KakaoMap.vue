f679799f5420044d197cec51b88da1e5552ecdaf
Kongjihun <wlgns3914@naver.com>
2022-05-23 21:32:57 +0900

    우측 필터 버튼

--- frontend/src/components/House/KakaoMap.vue
+++ frontend/src/components/House/KakaoMap.vue
@@ -4,6 +4,32 @@
       <span @click="zoomIn"><img :src="inImg" /></span>
       <span @click="zoomOut"><img :src="outImg" /></span>
     </div>
+    <ul id="category">
+      <li id="BK9" data-order="0">
+        <span class="category_bg bank"></span>
+        은행
+      </li>
+      <li id="MT1" data-order="1">
+        <span class="category_bg mart"></span>
+        마트
+      </li>
+      <li id="PM9" data-order="2">
+        <span class="category_bg pharmacy"></span>
+        약국
+      </li>
+      <li id="OL7" data-order="3">
+        <span class="category_bg oil"></span>
+        주유소
+      </li>
+      <li id="CE7" data-order="4">
+        <span class="category_bg cafe"></span>
+        카페
+      </li>
+      <li id="CS2" data-order="5">
+        <span class="category_bg store"></span>
+        편의점
+      </li>
+    </ul>
   </div>
 </template>
 
@@ -22,6 +48,11 @@ export default {
       outImg:
         "https://t1.daumcdn.net/localimg/localimages/07/mapapidoc/ico_minus.png",
       ...mapGetters(houseStore, ["houses"]),
+      markers: [],
+      placeOverlay: null,
+      currCategory: "",
+      ps: null,
+      contentNode: null,
     };
   },
   mounted() {
@@ -44,6 +75,179 @@ export default {
         });
       }
     },
+    createAroundInfo() {
+      this.contentNode = document.createElement("div");
+      kakao.maps.event.addListener(this.map, "idle", this.searchPlaces);
+      this.contentNode.className = "placeinfo_wrap";
+      this.addEventHandle(
+        this.contentNode,
+        "mousedown",
+        kakao.maps.event.preventMap
+      );
+      this.addEventHandle(
+        this.contentNode,
+        "touchstart",
+        kakao.maps.event.preventMap
+      );
+      this.placeOverlay.setContent(this.contentNode);
+      this.addCategoryClickEvent();
+    },
+    addEventHandle(target, type, callback) {
+      if (target.addEventListener) {
+        target.addEventListener(type, callback);
+      } else {
+        target.attachEvent("on" + type, callback);
+      }
+    },
+    searchPlaces() {
+      if (!this.currCategory) {
+        return;
+      }
+      this.placeOverlay.setMap(null);
+      this.removeMarker();
+
+      this.ps.categorySearch(this.currCategory, this.placesSearchCB, {
+        useMapBounds: true,
+      });
+    },
+    placesSearchCB(data, status) {
+      if (status === kakao.maps.services.Status.OK) {
+        // 정상적으로 검색이 완료됐으면 지도에 마커를 표출합니다
+        this.displayPlaces(data);
+      } else if (status === kakao.maps.services.Status.ZERO_RESULT) {
+        // 검색결과가 없는경우 해야할 처리가 있다면 이곳에 작성해 주세요
+      } else if (status === kakao.maps.services.Status.ERROR) {
+        // 에러로 인해 검색결과가 나오지 않은 경우 해야할 처리가 있다면 이곳에 작성해 주세요
+      }
+    },
+    displayPlaces(places) {
+      // 몇번째 카테고리가 선택되어 있는지 얻어옵니다
+      // 이 순서는 스프라이트 이미지에서의 위치를 계산하는데 사용됩니다
+      var order = document
+        .getElementById(this.currCategory)
+        .getAttribute("data-order");
+      for (var i = 0; i < places.length; i++) {
+        // 마커를 생성하고 지도에 표시합니다
+        var marker = this.addMarker(
+          new kakao.maps.LatLng(places[i].y, places[i].x),
+          order
+        );
+        // 마커와 검색결과 항목을 클릭 했을 때
+        // 장소정보를 표출하도록 클릭 이벤트를 등록합니다
+        ((marker, place) => {
+          kakao.maps.event.addListener(marker, "click", () => {
+            this.displayPlaceInfo(place);
+          });
+        })(marker, places[i]);
+      }
+    },
+    addMarker(position, order) {
+      var imageSrc =
+          "https://t1.daumcdn.net/localimg/localimages/07/mapapidoc/places_category.png", // 마커 이미지 url, 스프라이트 이미지를 씁니다
+        imageSize = new kakao.maps.Size(27, 28), // 마커 이미지의 크기
+        imgOptions = {
+          spriteSize: new kakao.maps.Size(72, 208), // 스프라이트 이미지의 크기
+          spriteOrigin: new kakao.maps.Point(46, order * 36), // 스프라이트 이미지 중 사용할 영역의 좌상단 좌표
+          offset: new kakao.maps.Point(11, 28), // 마커 좌표에 일치시킬 이미지 내에서의 좌표
+        },
+        markerImage = new kakao.maps.MarkerImage(
+          imageSrc,
+          imageSize,
+          imgOptions
+        ),
+        marker = new kakao.maps.Marker({
+          position: position, // 마커의 위치
+          image: markerImage,
+        });
+      marker.setMap(this.map); // 지도 위에 마커를 표출합니다
+      this.markers.push(marker); // 배열에 생성된 마커를 추가합니다
+
+      return marker;
+    },
+    removeMarker() {
+      for (var i = 0; i < this.markers.length; i++) {
+        this.markers[i].setMap(null);
+      }
+      this.markers = [];
+    },
+    displayPlaceInfo(place) {
+      var content =
+        '<div class="placeinfo">' +
+        '   <a class="title" href="' +
+        place.place_url +
+        '" target="_blank" title="' +
+        place.place_name +
+        '">' +
+        place.place_name +
+        "</a>";
+
+      if (place.road_address_name) {
+        content +=
+          '    <span title="' +
+          place.road_address_name +
+          '">' +
+          place.road_address_name +
+          "</span>" +
+          '  <span class="jibun" title="' +
+          place.address_name +
+          '">(지번 : ' +
+          place.address_name +
+          ")</span>";
+      } else {
+        content +=
+          '    <span title="' +
+          place.address_name +
+          '">' +
+          place.address_name +
+          "</span>";
+      }
+
+      content +=
+        '    <span class="tel">' +
+        place.phone +
+        "</span>" +
+        "</div>" +
+        '<div class="after"></div>';
+
+      this.contentNode.innerHTML = content;
+      this.placeOverlay.setPosition(new kakao.maps.LatLng(place.y, place.x));
+      this.placeOverlay.setMap(this.map);
+    },
+    addCategoryClickEvent() {
+      var category = document.getElementById("category"),
+        children = category.children;
+      for (var i = 0; i < children.length; i++) {
+        let id = children[i].id,
+          className = children[i].className;
+        children[i].addEventListener("click", () => {
+          this.placeOverlay.setMap(null);
+
+          if (className === "on") {
+            this.currCategory = "";
+            this.changeCategoryClass();
+            this.removeMarker();
+          } else {
+            this.currCategory = id;
+            this.changeCategoryClass();
+            this.searchPlaces();
+          }
+        });
+      }
+    },
+
+    changeCategoryClass(el) {
+      var category = document.getElementById("category"),
+        children = category.children,
+        i;
+
+      for (i = 0; i < children.length; i++) {
+        children[i].className = "";
+      }
+
+      if (el) {
+        el.className = "on";
+      }
+    },
     initMap() {
       var container = document.getElementById("map");
       var options = {
@@ -55,6 +259,9 @@ export default {
       var marker = new kakao.maps.Marker({ position: this.map.getCenter() });
       marker.setMap(this.map);
       this.createMarker();
+      this.ps = new kakao.maps.services.Places(this.map);
+      this.placeOverlay = new kakao.maps.CustomOverlay({ zIndex: 1 });
+      this.createAroundInfo();
     },
     createMarker() {
       var activeId = null;
@@ -263,5 +470,147 @@ export default {
 .radius_border {
   border: 1px solid #919191;
   border-radius: 5px;
+}
+#category {
+  position: absolute;
+  top: 100px;
+  left: 10px;
+  padding: 0;
+  border-radius: 5px;
+  border: 1px solid #909090;
+  box-shadow: 0 1px 1px rgba(0, 0, 0, 0.4);
+  background: #fff;
+  overflow: hidden;
+  z-index: 2;
+}
+#category li {
+  float: left;
+  list-style: none;
+  width: 50px;
+  border-right: 1px solid #acacac;
+  padding: 6px 0;
+  text-align: center;
+  cursor: pointer;
+}
+#category li.on {
+  background: #eee;
+}
+#category li:hover {
+  background: #ffe6e6;
+  border-left: 1px solid #acacac;
+  margin-left: -1px;
+}
+#category li:last-child {
+  margin-right: 0;
+  border-right: 0;
+}
+#category li span {
+  display: block;
+  margin: 0 auto 3px;
+  width: 27px;
+  height: 28px;
+}
+#category li .category_bg {
+  background: url(https://t1.daumcdn.net/localimg/localimages/07/mapapidoc/places_category.png)
+    no-repeat;
+}
+#category li .bank {
+  background-position: -10px 0;
+}
+#category li .mart {
+  background-position: -10px -36px;
+}
+#category li .pharmacy {
+  background-position: -10px -72px;
+}
+#category li .oil {
+  background-position: -10px -108px;
+}
+#category li .cafe {
+  background-position: -10px -144px;
+}
+#category li .store {
+  background-position: -10px -180px;
+}
+#category li.on .category_bg {
+  background-position-x: -46px;
+}
+.map_wrap,
+.map_wrap * {
+  margin: 0;
+  padding: 0;
+  font-family: "Malgun Gothic", dotum, "돋움", sans-serif;
+  font-size: 12px;
+}
+.map_wrap {
+  position: relative;
+  width: 100%;
+  height: 350px;
+}
+.placeinfo_wrap {
+  position: absolute;
+  bottom: 28px;
+  left: -150px;
+  width: 300px;
+}
+.placeinfo {
+  position: relative;
+  width: 100%;
+  border-radius: 6px;
+  border: 1px solid #ccc;
+  border-bottom: 2px solid #ddd;
+  padding-bottom: 10px;
+  background: #fff;
+}
+.placeinfo:nth-of-type(n) {
+  border: 0;
+  box-shadow: 0px 1px 2px #888;
+}
+.placeinfo_wrap .after {
+  content: "";
+  position: relative;
+  margin-left: -12px;
+  left: 50%;
+  width: 22px;
+  height: 12px;
+  background: url("https://t1.daumcdn.net/localimg/localimages/07/mapapidoc/vertex_white.png");
+}
+.placeinfo a,
+.placeinfo a:hover,
+.placeinfo a:active {
+  color: #fff;
+  text-decoration: none;
+}
+.placeinfo a,
+.placeinfo span {
+  display: block;
+  text-overflow: ellipsis;
+  overflow: hidden;
+  white-space: nowrap;
+}
+.placeinfo span {
+  margin: 5px 5px 0 5px;
+  cursor: default;
+  font-size: 13px;
+}
+.placeinfo .title {
+  font-weight: bold;
+  font-size: 14px;
+  border-radius: 6px 6px 0 0;
+  margin: -1px -1px 0 -1px;
+  padding: 10px;
+  color: #fff;
+  background: #d95050;
+  background: #d95050
+    url(https://t1.daumcdn.net/localimg/localimages/07/mapapidoc/arrow_white.png)
+    no-repeat right 14px center;
+}
+.placeinfo .tel {
+  color: #0f7833;
+}
+.placeinfo .jibun {
+  color: #999;
+  font-size: 11px;
+  margin-top: 0;
 }
 </style>
