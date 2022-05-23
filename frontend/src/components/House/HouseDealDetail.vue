f1d296239361ba911463326c567af36d82560684
yerimki <55068901+yerimki@users.noreply.github.com>
2022-05-23 13:11:51 +0900

    Update HouseDealDetail.vue
    검색결과 클릭시 거래내역 모달창 추가

--- frontend/src/components/House/HouseDealDetail.vue
+++ frontend/src/components/House/HouseDealDetail.vue
@@ -1,54 +1,61 @@
 <template>
-  <!-- [문제]
-   모달창 꺼질때 state 변경,
-   최초 클릭만 모달창 뜸 
-  -->
   <b-modal
     v-model="isSelectedHouse"
-    v-if="selectedHouse"
+    v-if="getSelectedHouse"
     id="deal-detail"
     title="상세정보"
+    @close="closeModal"
+    @ok="closeModal"
+    ok-only
   >
     <div class="row my-1">
       <span class="infoKey col-4">아파트명</span>
-      <span class="infoValue">{{ selectedHouse.aptName }}</span>
+      <span class="infoValue">{{ getSelectedHouse.aptName }}</span>
     </div>
     <div class="row my-1">
       <span class="infoKey col-4">건축년도</span>
-      <span class="infoValue">{{ selectedHouse.buildYear }}</span>
+      <span class="infoValue">{{ getSelectedHouse.buildYear }}</span>
     </div>
     <div class="row my-1">
       <span class="infoKey col-4">주소</span>
       <span class="infoValue">{{
-        selectedHouse.sidoName +
+        getSelectedHouse.sidoName +
         " " +
-        selectedHouse.gugunName +
+        getSelectedHouse.gugunName +
         " " +
-        selectedHouse.jibun
+        getSelectedHouse.jibun
       }}</span>
     </div>
     <div class="row my-1">
       <span class="infoKey col-4">최신거래금액</span>
-      <span class="infoValue">{{ selectedHouse.recentPrice }}</span>
+      <span class="infoValue">{{ getSelectedHouse.recentPrice }}</span>
     </div>
   </b-modal>
 </template>
 
 <script>
-import { mapState, mapMutations } from "vuex";
+import { mapMutations, mapGetters } from "vuex";
 const houseStore = "houseStore";
 
 export default {
   name: "HouseDealDetail",
   computed: {
-    ...mapState(houseStore, ["selectedHouse", "isSelectedHouse"]),
+    ...mapGetters(houseStore, ["getIsSelectedHouse", "getSelectedHouse"]),
+    isSelectedHouse: {
+      get() {
+        return this.getIsSelectedHouse;
+      },
+      set(bool) {
+        return bool;
+      },
+    },
   },
   methods: {
     ...mapMutations(houseStore, ["SET_SELECTEDHOUSE", "SET_ISSELECTEDHOUSE"]),
-  },
-  created() {
-    console.log(this.isSelectedHouse);
-    console.log(this.selectedHouse);
+    closeModal() {
+      this.SET_SELECTEDHOUSE(null);
+      this.SET_ISSELECTEDHOUSE(false);
+    },
   },
 };
 </script>
